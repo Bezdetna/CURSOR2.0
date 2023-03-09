e786c1ea22907a91a0ac6eeeb3a681045f536cd5
Alisa Bezdietna <bezdetnayaalisa@gmail.com>
2023-03-09 14:52:55 +0200

    add homework2

---
+++ Homework2/script.js
@@ -0,0 +1,34 @@
+const firstNumber = parseFloat(prompt("Choose a number N", ''))
+const firstNumberInt = Number.isInteger(firstNumber)
+
+while(!Number.isInteger(firstNumber)){
+    firstNumber = parseFloat(prompt("Choose a integer number N", ''))
+    firstNumberInt = Number.isInteger(firstNumber)
+}
+console.log(firstNumber)
+
+const secondNumber = parseFloat(prompt("Choose a number M", ''))
+const secondNumberInt = Number.isInteger(secondNumber)
+
+while(!Number.isInteger(secondNumber)){
+    secondNumber = parseFloat(prompt("Choose a integer number M", ''))
+    secondNumberInt = Number.isInteger(secondNumber)
+}
+console.log(secondNumber)
+
+const intNumberBol = confirm('Чи потрібно пропускати парні числа?')
+let sum = 0
+
+for(let i = firstNumber; i <= secondNumber; i++){
+    if(intNumberBol == true){
+        if(i % 2){
+            sum += i 
+        }
+    }else{
+        sum += i
+    }
+}
+console.log(sum)
+
+
+
---
+++ test/script.js
@@ -0,0 +1,147 @@
+/*1
+let quantity = prompt ("Введіть 1 число з клавіатури")
+if (quantity == 300){
+    alert("відсоси у тракториста")
+}else{
+    alert("Дякую.")
+}*/
+
+/*2
+let first = prompt ("Введіть 1 число з клавіатури")
+let second = prompt ("Введіть 2 число з клавіатури")
+if (first > second){
+     alert('1 число більше за 2')
+}else{
+    alert('2 число більше за 1')
+}*/
+
+/*3
+let number = prompt ("Введіть число(любе)")
+for (let i = 0; i <= number; i++) {
+    console.log("зараз число дорівінює " + i)
+}*/
+
+/*4
+ let number = prompt ("Введіть число(любе)")
+ let numberFirst = number
+ for (let i = 0; i <= number; i++) {
+    if(numberFirst == i){
+        console.log("хіхі хаха")
+    } else{
+        console.log("зараз число дорівінює " + i)
+    }
+}*/
+
+/*5
+let chooseNumber = prompt("Введіть щось(текст, символ, цифра, число)")
+let juga = chooseNumber
+   console.log( Number.isInteger(chooseNumber))
+
+if(Number.isInteger(juga)){
+    console.log('Ціле')
+}else{
+    console.log("Хер пойим шо введено")
+}*/
+
+/*6
+let x = prompt ('Введіть ціле число')
+let y = parseFloat (x)
+let z = Number.isInteger(y)
+console.log( Number.isInteger(y))
+while(!z){
+   x = prompt("Ти шо ідіот ціле число введи")
+   y = parseFloat (x)
+   z = Number.isInteger(y)
+   console.log(z)
+}
+alert('thanks')
+*/
+
+/*7 
+let firstName = prompt("Введіть з клавіатури Ім'я")
+
+let lastName = prompt("Введіть з клавіатури Прізвище")
+
+let age = prompt("Введіть з клавіатури вік")
+
+let ageNumber = parseInt(age)
+
+    if( ageNumber >= 25) {
+        console.log( firstName, lastName + ' Ого ти дєд')
+    } else {
+        console.log( firstName, lastName + ' малалєтка')};
+
+    if( firstName == "Євхен" && lastName == "Бурдун"){ 
+        console.log( firstName, lastName + " є лох")
+    }*/
+
+/*8 
+let firsValue = prompt('Введіть з клавіатури що небудь (текст, букву, цифру, число)')
+
+let secondValue = prompt('Введіть з клавіатури що небудь (текст, букву, цифру, число)')
+
+   if( x == y ){
+    console.log("Значення рівні")
+   }else{
+    console.log("Значення не рівні")
+   }*/
+
+/*9
+var x = prompt('Введіть з клавіатури цифру, aбо число')
+var y = prompt('Введіть з клавіатури цифру, aбо число')
+let a = parseInt(x)
+let b = parseInt(y)
+const d = b + 5
+const z = a * 15
+const k = a / 8
+const n = a % b
+
+if(a > 10 ){
+    console.log(a +" більше 10")
+}else if (a < 10){
+    console.log(a + " менше 10")
+}else if(a == 10){
+    console.log(a + " дорівнює 10")
+}
+
+if(b > 13 ){
+    console.log(b +" більше 13")
+}
+ if(d > 13){
+    console.log(b + " + 5 = " + d + " більше 13")
+}
+console.log( a + " * 15 = " + z)
+console.log( a + " / 8 = " + k)
+console.log( "остача від ділення " + a +" на "+ b + " дорівнює " + n)*/
+
+/*10
+let y = 28;
+let i = 0;
+
+for( i = 0; i <= y; i++){
+    if(i % 2){
+        console.log( 'не парне ' + i)
+    }else{
+        console.log( 'парне ' + i)
+    }if(i == 0){
+        console.log( 'Нуль ')
+    }
+
+}*/
+
+/*11
+let a = prompt('Введдіть з клавіатури цифру або число')
+let b = parseFloat(a)
+let c = Number.isInteger(b)
+
+if(c){
+    console.log("Ціле")
+}else{
+    console.log("Або число не ціле, або ви ввели якусь херню")
+}
+
+console.log(c)*/
+
+
+
+
