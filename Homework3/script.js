5d99bbf9104a40948efb848c9dce61155cd01100
Alisa Bezdietna <bezdetnayaalisa@gmail.com>
2023-03-14 17:58:27 +0200

    add homework3

--- Homework3/script.js
+++ Homework3/script.js
@@ -1,17 +1,55 @@
-/*function getMaxDigit(number){
-    const maxValue = Math.max(...number.toString())
-    console.log(maxValue)
-  }
-    const number = prompt('Ведіть число')
-    getMaxDigit(number)*/
-
-
-const number = prompt('Ведіть число')
-let num = 0
-
-for(let i = 0; i < number.length; i++){
-    if( num > i){
-        num = i
-    }
-        console.log( number[i])
+// Функція, яка отримує будь-яке число та виводить найбільшу цифру в цьому числі.
+function getMaxDigit(number){
+    let getMaxDigit = Math.max(...number.toString().split(""))
+    console.log(`Найбільша цифра у числі ${number}: ${getMaxDigit}`)
 }
+getMaxDigit(74574549)
+
+// Функцію, яка визначає ступінь числа.
+function pow(a, b) {
+    let result = a;
+    for (let i = 1; i < b; i++) {
+      result *= a;
+    }
+    return result;
+  }
+console.log(pow(2, 3))
+
+// Функція, яка форматує ім'я, роблячи першу букву великою.
+function uppercase(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
+  }
+  console.log(uppercase("alisa"))
+
+
+// Функція, яка повертає випадкове ціле число
+function getRandom(N, M){
+    if(N % 1 === 0 && M % 1 === 0){
+        return Math.floor(Math.random() * (M - N + 1) + N)
+    }else{
+        alert("Ввели не цілі числа")
+    }
+    
+}
+console.log(getRandom(10, 20))
+
+
+//Функція, яка рахує скільки разів певна буква повторюється в слові.
+let sum = 0
+function countLetter(word,letter){
+        for(let i = 0; i < word.length; i++){
+            if(word[i] === letter){
+                sum++
+            }
+        }
+    }
+countLetter("Розгніваний", "н")
+console.log(sum)
+
+//Функція, яка перевіряє, чи є слово паліндромом.
+function isPalindrome(str) {
+    const reversedStr = str.split('').reverse().join(''); // перетворюємо рядок на масив символів, обертаємо його та зливаємо знову в рядок
+    return str === reversedStr;// порівнюємо обернутий рядок з оригінальним рядком
+}
+let wordPalindrom = isPalindrome("ротатор")
+console.log(wordPalindrom)
